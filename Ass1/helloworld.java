cc570393a4ca95cb2bbd54ca80760f3d76183d62
sang <pvsang8022002@gmail.com>
2025-08-18 00:28:44 +0700

    add example java code

---
+++ Ass1/helloworld.java
@@ -0,0 +1,8 @@
+// example java
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+// This is a simple Java program that prints "Hello, World!" to the console.
+// It serves as a basic example of Java syntax and structure.
